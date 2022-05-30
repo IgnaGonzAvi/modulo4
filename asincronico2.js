d2e9f648b35d6b3d4d5340b1e6bc938f0dd7bab1
IgnaGonzAvi <103262229+IgnaGonzAvi@users.noreply.github.com>
2022-05-30 13:58:13 -0400

    Create asincronico2.js

---
+++ asincronico2.js
@@ -0,0 +1,37 @@
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    background-color: darkblue;
+    content: center;
+};
+
+h1 {
+    background-color: white;
+};
+
+.input-div {
+ width: 450px;
+ height: 50px;
+ margin: 5px;
+ padding-top: 0px;
+ padding-bottom: 0px;
+ padding-left: 20px;
+ padding-right: 20px;
+ border: 0px;
+ border-radius: 10px;
+ background-color: white;
+ font-size: 18px;
+};
+
+.boton-agregar {
+ width: 50px;
+ height: 50px;
+ margin: 5px;
+ border: 0px;
+ border-radius: 10px;
+ background-color: rgb(255, 6, 46);
+ color: white;
+};
+
+.boton-agregar:hover {
+    opacity: 0.9;
+}
